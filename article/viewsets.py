2d724f8edf9aac29f758aec8a089a2db9ff73afb
Samuel Dias <samuelcddo@gmail.com>
2018-08-28 11:33:59 -0300

    first commit

---
+++ article/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from article.models import Article
+from article.serializers import ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
